1a5229b11baff14b95d2aafdb387b19efc36ab4e
Aisha Almas <aishalmas786@email.com>
2025-07-24 03:31:06 -0700

    CrawlADomain edit

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
 import SearchMessages from "../pages/Tags/Tags Search/SearchMessages";
 import SearchItems from "../pages/Tags/Tags Search/SearchItems";
+import CrawlADomain from "../pages/crawlers/splash crawlers/CrawlADomain";
 
 
 
@@ -14,6 +15,7 @@ const AppRouter = () => {
         <Route path="/" element={<Home />} />
         <Route path="/SearchItemsByTags" element={< SearchItems/>} />
         <Route path="/SearchMessagesByTags" element={<SearchMessages />} />
+        <Route path="/CrawlADomain" element={<CrawlADomain />} />
       </Routes>
     </BrowserRouter>
   )
